ed93524f49bbe5321a5175dc15fcf1bd3c615d85
Chris <chrislan@protonmail.com>
2021-03-12 13:37:25 +0200

    added img

--- client/src/components/pages/Landing.js
+++ client/src/components/pages/Landing.js
@@ -17,7 +17,10 @@ const Landing = () => {
   return (
     <section className='landing'>
       <nav className='top'>
-        <h2> PlanIt</h2>
+        <h2> 
+        <img src="../img/logo.png"/>
+          PlanIt
+          </h2>
         <div>
           <Button color='inherit' href='/login'>
             Login
