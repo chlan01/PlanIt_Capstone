a1164e45269f656a532c02d91033bbcce5a3d1f5
Chris <chrislan@protonmail.com>
2021-03-12 12:23:08 +0200

    changed background image

--- client/src/components/pages/Board.js
+++ client/src/components/pages/Board.js
@@ -61,7 +61,7 @@ const Board = ({ match }) => {
           'url(' +
           (board.backgroundURL
             ? board.backgroundURL
-            : 'https://images.unsplash.com/photo-1598197748967-b4674cb3c266?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2689&q=80') +
+            : 'https://images.unsplash.com/photo-1579033461380-adb47c3eb938?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80') +
           ')',
       }}
     >
