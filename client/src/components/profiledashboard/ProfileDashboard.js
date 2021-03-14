02aef49e3a11f3757cba1f5bd9a842269832fdd7
Chris <chrislan@protonmail.com>
2021-03-14 14:14:31 +0200

    restored prof dash

--- client/src/components/profiledashboard/ProfileDashboard.js
+++ client/src/components/profiledashboard/ProfileDashboard.js
@@ -2,50 +2,16 @@ import React, { Fragment, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import DashboardActions from './DashboardActions';
-import Experience from './Experience';
-import Education from './Education';
-import { getCurrentProfile, deleteAccount } from '../../actions/profile';
 
-const ProfileDashboard = ({
-  getCurrentProfile,
-  deleteAccount,
-  auth: { user },
-  profile: { profile }
-}) => {
-  useEffect(() => {
-    getCurrentProfile();
-  }, [getCurrentProfile]);
+const ProfileDashboard = ({ getCurrentProfile, auth, profile }) => {
+    useEffect(() => {
+        getCurrentProfile();
+    }, []);
 
-  return (
-    <Fragment>
-      <h1 className="large text-primary">Dashboard</h1>
-      <p className="lead">
-        <i className="fas fa-user" /> Welcome {user && user.name}
-      </p>
-      {profile !== null ? (
-        <Fragment>
-          <DashboardActions />
-          <Experience experience={profile.experience} />
-          <Education education={profile.education} />
+    return <div>Profile Dashboard</div>
+}
 
-          <div className="my-2">
-            <button className="btn btn-danger" onClick={() => deleteAccount()}>
-              <i className="fas fa-user-minus" /> Delete My Account
-            </button>
-          </div>
-        </Fragment>
-      ) : (
-        <Fragment>
-          <p>You have not yet setup a profile, please add some info</p>
-          <Link to="/create-profile" className="btn btn-primary my-1">
-            Create Profile
-          </Link>
-        </Fragment>
-      )}
-    </Fragment>
-  );
-};
+
 
 ProfileDashboard.propTypes = {
   getCurrentProfile: PropTypes.func.isRequired,
