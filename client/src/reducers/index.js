f1f41b5092cd56d4d2d62f22b5123a9a9678d072
Chris <chrislan@protonmail.com>
2021-03-15 22:24:16 +0200

    updated actions & reducer for posts

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,12 +2,12 @@ import { combineReducers } from 'redux';
 import alert from './alert';
 import auth from './auth';
 import board from './board';
-import profile from './profile';
+import post from './post';
 
 export default combineReducers 
 ({ 
     alert, 
     auth, 
     board,
-    profile,
+    post
 });
