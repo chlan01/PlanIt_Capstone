1d69ee57f644c7278d103e4deb717565cc775ed4
Chris <chrislan@protonmail.com>
2021-03-17 21:08:59 +0200

    reducer changes

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,13 +1,15 @@
 import { combineReducers } from 'redux';
 import alert from './alert';
-import auth from './auth';
+import auth from './auth(Old)';
 import board from './board';
 import post from './post';
+import profile from './profile';
 
 export default combineReducers 
 ({ 
     alert, 
     auth, 
     board,
-    post
+    post,
+    profile
 });
